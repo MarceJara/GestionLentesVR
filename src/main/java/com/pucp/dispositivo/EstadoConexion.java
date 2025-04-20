bede2ce81fd0e7274397795ed50118e48851a626
axelsmith11 <amestanza@pucp.edu.pe>
2025-04-20 01:29:18 -0500

    Agregado Capa Dominio Axel

---
+++ src/main/java/com/pucp/dispositivo/EstadoConexion.java
@@ -0,0 +1,11 @@
+package com.pucp.dispositivo;
+
+/**
+ * Enumeración que representa los posibles estados de conexión de un dispositivo VR
+ */
+public enum EstadoConexion {
+    CONECTADO,
+    DESCONECTADO,
+    EN_USO,
+    EN_MANTENIMIENTO
+}
---
+++ src/main/java/com/pucp/presentacion/GestionVRAPP.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pucp.presentacion;
+
+/**
+ *
+ * @author Administrador
+ */
+public class GestionVRAPP {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
