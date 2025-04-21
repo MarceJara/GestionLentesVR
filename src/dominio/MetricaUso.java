9ff19e0984127dedb5352fb901e3a4d4c3a28b92
marcelo.jara <marcelo.jara@pucp.edu.pe>
2025-04-21 10:54:27 -0500

    fix metricas de uso

--- src/dominio/MetricaUso.java
+++ src/dominio/MetricaUso.java
@@ -27,4 +27,119 @@ public class MetricaUso {
         this.dispositivo = dispositivo;
         this.tiempoUsoMinutos = tiempoUsoMinutos;
         this.aplicacionUsada = aplicacionUsada;
-        this.nivelBateriaInicial = nivelBateriaIn
+    private int nivelBateriaInicial;
+    private int nivelBateriaFinal;
+    private Usuario usuario;
+    
+    public MetricaUso() {
+        this.fechaRegistro = new Date();
+    }
+    
+    public MetricaUso(int metricaId, Dispositivo dispositivo, int tiempoUsoMinutos, 
+                     Aplicacion aplicacionUsada, int nivelBateriaInicial, 
+                     int nivelBateriaFinal, Usuario usuario) {
+        this();
+        this.metricaId = metricaId;
+        this.dispositivo = dispositivo;
+        this.tiempoUsoMinutos = tiempoUsoMinutos;
+        this.aplicacionUsada = aplicacionUsada;
+        this.nivelBateriaInicial = nivelBateriaInicial;
+        this.nivelBateriaFinal = nivelBateriaFinal;
+        this.usuario = usuario;
+    }
+    
+    // Getters y Setters
+    public int getMetricaId() {
+        return metricaId;
+    }
+    
+    public void setMetricaId(int metricaId) {
+        this.metricaId = metricaId;
+    }
+    
+    public Dispositivo getDispositivo() {
+        return dispositivo;
+    }
+    
+    public void setDispositivo(Dispositivo dispositivo) {
+        this.dispositivo = dispositivo;
+    }
+    
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+    
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+    
+    public int getTiempoUsoMinutos() {
+        return tiempoUsoMinutos;
+    }
+    
+    public void setTiempoUsoMinutos(int tiempoUsoMinutos) {
+        this.tiempoUsoMinutos = tiempoUsoMinutos;
+    }
+    
+    public Aplicacion getAplicacionUsada() {
+        return aplicacionUsada;
+    }
+    
+    public void setAplicacionUsada(Aplicacion aplicacionUsada) {
+        this.aplicacionUsada = aplicacionUsada;
+    }
+    
+    public int getNivelBateriaInicial() {
+        return nivelBateriaInicial;
+    }
+    
+    public void setNivelBateriaInicial(int nivelBateriaInicial) {
+        this.nivelBateriaInicial = nivelBateriaInicial;
+    }
+    
+    public int getNivelBateriaFinal() {
+        return nivelBateriaFinal;
+    }
+    
+    public void setNivelBateriaFinal(int nivelBateriaFinal) {
+        this.nivelBateriaFinal = nivelBateriaFinal;
+    }
+    
+    public Usuario getUsuario() {
+        return usuario;
+    }
+    
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+    
+    /**
+     * Calcula el consumo de batería durante el uso
+     * @return Porcentaje de batería consumido
+     */
+    public int calcularConsumoBateria() {
+        return nivelBateriaInicial - nivelBateriaFinal;
+    }
+    
+    /**
+     * Verifica si el uso fue prolongado (más de 60 minutos)
+     * @return true si el uso fue prolongado, false en caso contrario
+     */
+    public boolean esUsoProlongado() {
+        return tiempoUsoMinutos > 60;
+    }
+    
+    @Override
+    public String toString() {
+        return "MetricaUso{" +
+                "metricaId=" + metricaId +
+                ", dispositivo=" + dispositivo +
+                ", fechaRegistro=" + fechaRegistro +
+                ", tiempoUsoMinutos=" + tiempoUsoMinutos +
+                ", aplicacionUsada=" + aplicacionUsada +
+                ", nivelBateriaInicial=" + nivelBateriaInicial +
+                ", nivelBateriaFinal=" + nivelBateriaFinal +
+                ", usuario=" + usuario +
+                '}';
+    }
+}
