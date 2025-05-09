5c78a422107f70d5dfbd571395cdf2bc915e324b
CosmicCooter <oscarlau.734@gmail.com>
2025-05-09 02:03:03 -0500

    commit_Procedures
    procedures for DAOs

---
+++ sqlDB/dropProcedures.sql
@@ -0,0 +1,58 @@
+DELIMITER //
+
+-- Drops para las tablas 
+DROP PROCEDURE IF EXISTS insertar_usuario;
+DROP PROCEDURE IF EXISTS actualizar_usuario;
+DROP PROCEDURE IF EXISTS eliminar_usuario;
+DROP PROCEDURE IF EXISTS listar_usuarios;
+DROP PROCEDURE IF EXISTS obtener_usuario;
+
+DROP PROCEDURE IF EXISTS insertar_grupo;
+DROP PROCEDURE IF EXISTS actualizar_grupo;
+DROP PROCEDURE IF EXISTS eliminar_grupo;
+DROP PROCEDURE IF EXISTS listar_grupo;
+DROP PROCEDURE IF EXISTS obtener_grupo;
+
+DROP PROCEDURE IF EXISTS insertar_dispositivo;
+DROP PROCEDURE IF EXISTS actualizar_dispositivo;
+DROP PROCEDURE IF EXISTS eliminar_dispositivo;
+DROP PROCEDURE IF EXISTS listar_dispositivo;
+DROP PROCEDURE IF EXISTS obtener_dispositivo;
+
+DROP PROCEDURE IF EXISTS insertar_actividad;
+DROP PROCEDURE IF EXISTS actualizar_actividad;
+DROP PROCEDURE IF EXISTS eliminar_actividad;
+DROP PROCEDURE IF EXISTS listar_actividad;
+DROP PROCEDURE IF EXISTS obtener_actividad;
+
+DROP PROCEDURE IF EXISTS insertar_aplicacion;
+DROP PROCEDURE IF EXISTS actualizar_aplicacion;
+DROP PROCEDURE IF EXISTS eliminar_aplicacion;
+DROP PROCEDURE IF EXISTS listar_aplicacion;
+DROP PROCEDURE IF EXISTS obtener_aplicacion;
+
+DROP PROCEDURE IF EXISTS insertar_configuracion;
+DROP PROCEDURE IF EXISTS actualizar_configuracion;
+DROP PROCEDURE IF EXISTS eliminar_configuracion;
+DROP PROCEDURE IF EXISTS listar_configuracion;
+DROP PROCEDURE IF EXISTS obtener_configuracion;
+
+DROP PROCEDURE IF EXISTS insertar_firmware;
+DROP PROCEDURE IF EXISTS actualizar_firmware;
+DROP PROCEDURE IF EXISTS eliminar_firmware;
+DROP PROCEDURE IF EXISTS listar_firmware;
+DROP PROCEDURE IF EXISTS obtener_firmware;
+
+DROP PROCEDURE IF EXISTS insertar_metricauso;
+DROP PROCEDURE IF EXISTS actualizar_metricauso;
+DROP PROCEDURE IF EXISTS eliminar_metricauso;
+DROP PROCEDURE IF EXISTS listar_metricauso;
+DROP PROCEDURE IF EXISTS obtener_metricauso;
+
+DROP PROCEDURE IF EXISTS insertar_rol;
+DROP PROCEDURE IF EXISTS actualizar_rol;
+DROP PROCEDURE IF EXISTS eliminar_rol;
+DROP PROCEDURE IF EXISTS listar_rol;
+DROP PROCEDURE IF EXISTS obtener_rol;
+
+DELIMITER ;
