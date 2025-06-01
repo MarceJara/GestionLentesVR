1975c0c63de703e2acb0b2819c4ed60b5b8697c1
CosmicCooter <oscarlau.734@gmail.com>
2025-06-01 13:36:03 -0500

    Commit Services

---
+++ GestionLentesVRNegocio/src/main/java/com/pucp/gestionlentesvr/negocioimpl/FirmwareServiceImpl.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pucp.gestionlentesvr.negocioimpl;
+
+import com.pucp.gestionlentesvr.negocio.*;
+
+/**
+ *
+ * @author oscar
+ */
+public class FirmwareServiceImpl {
+    
+}
---
+++ GestionLentesVRWebServices/src/main/java/com/pucp/gestionlentesvr/ws/ActividadWS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/WebServices/WebService.java to edit this template
+ */
+package com.pucp.gestionlentesvr.ws;
+
+import com.pucp.gestionlentesvr.negocio.ActividadService;
+import jakarta.jws.WebService;
+import jakarta.jws.WebMethod;
+import jakarta.jws.WebParam;
+
+/**
+ *
+ * @author oscar
+ */
+@WebService(serviceName = "ActividadWS", targetNamespace = "com.pucp.gestionlentesvr")
+public class ActividadWS {
+
+    private ActividadService service;
+    
+    public ActividadWS(){
+        service = new ActividadService
+    }
+    
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
