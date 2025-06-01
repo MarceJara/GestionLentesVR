aaa1566ef7f5c132cf31d72ebbcca3805bc1d770
CosmicCooter <oscarlau.734@gmail.com>
2025-06-01 14:52:38 -0500

    FixCommitServices

--- GestionLentesVRNegocio/src/main/java/com/pucp/gestionlentesvr/negocio/RolService.java
+++ GestionLentesVRNegocio/src/main/java/com/pucp/gestionlentesvr/negocio/RolService.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.pucp.gestionlentesvr.negocio;
-
+import com.pucp.gestionlentesvr.dominio.Rol;
+import java.util.List;
 /**
  *
  * @author oscar
  */
 public interface RolService {
-    
+
+    void registrarRol(Rol elemento) throws Exception;
+
+    void actualizarRol(Rol elemento) throws Exception;
+
+    void eliminarRol(int id) throws Exception;
+
+    Rol obtenerRol(int id) throws Exception;
+
+    List<Rol> listarRol() throws Exception;
+
 }
